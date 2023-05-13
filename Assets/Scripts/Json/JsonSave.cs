7360e0598d2a65978d60daacae21aaa9737cfbf6
Harun Aşık <73292463+harunesad@users.noreply.github.com>
2023-05-13 21:24:13 +0300

    json fixed

--- Assets/Scripts/Json/JsonSave.cs
+++ Assets/Scripts/Json/JsonSave.cs
@@ -4,12 +4,21 @@ public class JsonSave : MonoBehaviour
 {
     public static JsonSave jsonSave;
     public SaveVariable sv;
+    int firstOpen;
+    string firstKey = "First";
     private void Awake()
     {
         jsonSave = this;
     }
     void Start()
     {
+        firstOpen = PlayerPrefs.GetInt(firstKey);
+        if (firstOpen == 0)
+        {
+            SaveManager.Save(sv);
+            firstOpen++;
+            PlayerPrefs.SetInt(firstKey, firstOpen);
+        }
         sv = SaveManager.Load();
     }
 }
